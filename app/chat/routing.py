7b5563b68bc5bd0c4d5c55d71b05b94b2b0047fc
parkhongbeen <pack122@naver.com>
2020-04-16 20:54:18 +0900

    메시지 보내짐

--- app/chat/routing.py
+++ app/chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from chat import consumers
 
 websocket_urlpatterns = [
-    path('ws/<str:room_name/', consumers.ChatConsumer),
+    path('ws/<str:room_name>/', consumers.ChatConsumer),
 ]
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hongbeen/projects/wps12th/channels/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
